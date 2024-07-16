e294d02c559674c803d2111a78a7b9a5cb31a4df
aaron <aaron@DESKTOP-1UH081U>
2024-07-15 21:49:19 -0600

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DairySync
+{
+
+
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Se establece usuario y contraseña para el login, si son correctas se crea el form 2 y se esconde el primero.
+            if (textBox1.Text == "user" && textBox2.Text == "equipo4")
+            {
+                Form f2 = new Form2();
+                f2.Show();
+                this.Hide();
+            }
+
+
+        }
+    }
+
+
+}
+
